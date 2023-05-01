c38fd3bef526f33e0b17f48958bdb4909616a5ef
dan.markhasin <dan.markhasin@appsflyer.com>
2023-05-01 12:01:11 +0300

    lint

--- test/utils.go
+++ test/utils.go
@@ -64,7 +64,7 @@ func deregisterServiceInConsul(name string, client *api.Client) error {
 	return client.Agent().ServiceDeregister(name)
 }
 
-func registerServiceInConsul(id int, name string, tags []string, client *api.Client) error { //nolint:unparam
+func registerServiceInConsul(id int, name string, tags []string, client *api.Client) error { //nolint:unparam too sensitive
 	return client.Agent().ServiceRegister(&api.AgentServiceRegistration{
 		ID:      fmt.Sprintf("%d", id),
 		Name:    name,
